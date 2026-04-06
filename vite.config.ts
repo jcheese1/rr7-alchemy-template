a4515c88b4a9f3d7052162b0dc4c44d926918207
jcheese1 <15570714+jcheese1@users.noreply.github.com>
2026-04-06 13:23:14 +0900

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import alchemy from "alchemy/cloudflare/react-router";
 import { defineConfig, type PluginOption } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true
+  },
   plugins: [
     alchemy() as PluginOption,
     tailwindcss(),
     reactRouter(),
-    tsconfigPaths({
-      root: "."
-    }),
   ],
 });
